e3a71f5ae04e582c85325f9c91dcbcf307a16011
n4n5 <its.just.n4n5@gmail.com>
2023-11-07 13:47:46 +0100

    f

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
     url: "https://its-just-nans.github.io/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "./",
+    baseUrl: "/rio/",
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
 
