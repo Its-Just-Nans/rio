250a6dd7141de7dc6adbdfbcc51eb641639bd0f3
n4n5 <its.just.n4n5@gmail.com>
2024-05-24 17:25:30 +0200

    change config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,10 +18,10 @@ const config = {
         mermaid: true,
     },
     // Set the production url of your site here
-    url: "https://its-just-nans.github.io/",
+    url: "https://rio.n4n5.dev/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/rio/",
+    baseUrl: "/",
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
 
